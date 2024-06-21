14162c18f52ed03803776bbf9a196eb088d93cc1
Nuthelk <46610056+nuthelk@users.noreply.github.com>
2024-06-21 13:04:33 -0500

    add design items links

--- src/components/UploadFile.vue
+++ src/components/UploadFile.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
+  <div class="file-upload">
     <h1>Upload and List Files</h1>
     <input type="file" @change="handleFileUpload" />
     <button @click="uploadFile">Upload</button>
     <ul>
-      <li v-for="file in files" :key="file.fileName">
-        <a @click="downloadFile(file.fileName)">{{ file.fileName }}</a>
+      <li v-for="file in files" :key="file.fileName" class="file-item">
+        <a @click="downloadFile(file.fileName)" class="file-link">{{ file.fileName }}</a>
       </li>
     </ul>
   </div>
@@ -84,3 +84,41 @@ export default defineComponent({
   }
 });
 </script>
+
+<style scoped>
+.file-upload {
+  max-width: 600px;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.file-upload h1 {
+  font-size: 2em;
+  margin-bottom: 20px;
+}
+
+.file-upload input[type="file"] {
+  margin-bottom: 10px;
+}
+
+.file-upload button {
+  margin-bottom: 20px;
+}
+
+.file-item {
+  margin: 10px 0;
+  list-style: none;
+}
+
+.file-link {
+  color: #42b983;
+  text-decoration: none;
+  font-size: 1.2em;
+  transition: color 0.3s ease;
+  cursor:pointer;
+}
+
+.file-link:hover {
+  color: #185e3e;
+}
+</style>
